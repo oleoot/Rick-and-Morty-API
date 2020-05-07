71647be0a3ef11231aa48668c2ce3c1a3cdfaaed
oleoot <oleoot28@gmail.com>
2020-05-07 11:16:45 +0300

    Got more info from API

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ async function getCharacters() {
     const data = await response.json();
     const data_result = data.results
     data_result.forEach((character) => {
-        const { name, species, image } = character;
+        const { name, species, image, status, location } = character;
         let wrapper = document.querySelector('#wrapper');
         let container = document.createElement('div');
         let nameElement = document.createElement('p');
         let speciesElement = document.createElement('p');
         let imgElement = document.createElement('img');
+        let statusElement = document.createElement('p');
+        let locationElement = document.createElement('p');
         nameElement.innerText = name;
         speciesElement.innerText = species;
+        statusElement.innerText = status;
+        locationElement.innerText = location.name
         imgElement.src = image;
-        container.append(imgElement, nameElement, speciesElement)
+        container.append(imgElement, nameElement, speciesElement, locationElement, statusElement)
         wrapper.append(container)
     });
 
